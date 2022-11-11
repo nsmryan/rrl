c289c855695fc2b3d40e68bc9a43023c06ec2cfa
Noah Ryan <nsmryan@gmail.com>
2022-11-11 13:07:13 -0500

    Added tcl script to modify atlas output to add tile locations

---
+++ scripts/add_tiles_to_atlas.tcl
@@ -0,0 +1,30 @@
+lassign $::argv spriteAtlasName tileLocationsName finalAtlasName
+
+set spriteFile [open $spriteAtlasName r]
+set tileLocationsFile [open $tileLocationsName r]
+
+set spriteLines [split [string trim [read $spriteFile]] "\n"]
+set tileLocations [read $tileLocationsFile]
+
+foreach line $spriteLines {
+    dict set sprites [lindex $line 0] [lrange $line 1 end]
+}
+lassign [dict get $sprites rustrogueliketiles] x y width height
+
+set dim 16
+dict for {key value} $tileLocations {
+    set x_offset [expr $dim * ($key % 16)]
+    set y_offset [expr $dim * ($key / 16)]
+    dict set sprites $value [list [expr $x + $x_offset] [expr $y + $y_offset] $dim $dim]
+}
+
+set finalAtlas [open $finalAtlasName w]
+dict for { key value } $sprites {
+    puts $finalAtlas [concat $key $value]
+}
+close $finalAtlas
+
+
+close $spriteFile
+close $tileLocationsFile
+

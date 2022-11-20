fbc2876616ce958434272db9bfa3ee905de5cb75
Noah Ryan <nsmryan@gmail.com>
2022-11-20 12:21:34 -0500

    Combining enums works

--- src/core/entities.zig
+++ src/core/entities.zig
@@ -228,93 +228,100 @@ pub const EntityState = enum {
     remove,
 };
 
-// NOTE(implement) combining Item with extra names to create Names segfaults zig10 and crashes zig11.
-// See https://github.com/ziglang/zig/issues/13606 for status.
-//pub const Name = blk: {
-//    const numFields = @typeInfo(Item).Enum.fields.len + @typeInfo(ExtraNames).Enum.fields.len;
-//    comptime var fields: [numFields]std.builtin.Type.EnumField = undefined;
-//
-//    comptime var index = 0;
-//    for (std.meta.fields(Item)) |field| {
-//        fields[index] = field;
-//        index += 1;
-//    }
-//
-//    for (std.meta.fields(ExtraNames)) |field| {
-//        fields[index] = field;
-//        index += 1;
-//    }
-//
-//    const enumInfo = std.builtin.Type.Enum{
-//        .layout = std.builtin.Type.ContainerLayout.Auto,
-//        .tag_type = u8,
-//        .fields = &fields,
-//        .decls = &[0]std.builtin.Type.Declaration{},
-//        .is_exhaustive = true,
-//    };
-//
-//    break :blk @Type(std.builtin.Type{ .Enum = enumInfo });
-//};
-//
-//pub const ExtraNames = enum {
+// Entity names combine items and remaining entities.
+pub const Name = blk: {
+    const numFields = @typeInfo(Item).Enum.fields.len + @typeInfo(ExtraNames).Enum.fields.len + @typeInfo(Golems).Enum.fields.len;
+    comptime var fields: [numFields]std.builtin.Type.EnumField = undefined;
+
+    comptime var index = 0;
+    for (std.meta.fields(Item)) |field| {
+        fields[index] = std.builtin.Type.EnumField{ .name = field.name, .value = index };
+        index += 1;
+    }
+
+    for (std.meta.fields(ExtraNames)) |field| {
+        fields[index] = std.builtin.Type.EnumField{ .name = field.name, .value = index };
+        index += 1;
+    }
+
+    for (std.meta.fields(Golems)) |field| {
+        fields[index] = std.builtin.Type.EnumField{ .name = field.name, .value = index };
+        index += 1;
+    }
+
+    const enumInfo = std.builtin.Type.Enum{
+        .layout = std.builtin.Type.ContainerLayout.Auto,
+        .tag_type = u8,
+        .fields = &fields,
+        .decls = &[0]std.builtin.Type.Declaration{},
+        .is_exhaustive = true,
+    };
+
+    break :blk @Type(std.builtin.Type{ .Enum = enumInfo });
+};
+
+pub const Golems = enum {
+    gol,
+    pawn,
+    rook,
+    spire,
+    armil,
+};
+
+pub const ExtraNames = enum {
+    player,
+    column,
+    exit,
+    gateTrigger,
+    mouse,
+    cursor,
+    energy,
+    grass,
+    statue,
+    smoke,
+    other,
+};
+
+//pub const Name = enum {
 //    player,
 //    gol,
 //    pawn,
 //    rook,
 //    column,
+//    key,
 //    exit,
+//    dagger,
+//    hammer,
+//    spear,
+//    greatSword,
+//    sword,
+//    shield,
+//    lantern,
+//    thumper,
+//    axe,
+//    khopesh,
+//    sling,
+//    seedOfStone,
+//    seedCache,
+//    smokeBomb,
+//    lookingGlass,
+//    glassEye,
+//    teleporter,
 //    spire,
 //    armil,
+//    spikeTrap,
+//    blinkTrap,
+//    freezeTrap,
+//    soundTrap,
 //    gateTrigger,
+//    stone,
 //    mouse,
 //    cursor,
 //    energy,
+//    herb,
 //    grass,
 //    statue,
 //    smoke,
+//    magnifier,
 //    other,
 //};
-
-pub const Name = enum {
-    player,
-    gol,
-    pawn,
-    rook,
-    column,
-    key,
-    exit,
-    dagger,
-    hammer,
-    spear,
-    greatSword,
-    sword,
-    shield,
-    lantern,
-    thumper,
-    axe,
-    khopesh,
-    sling,
-    seedOfStone,
-    seedCache,
-    smokeBomb,
-    lookingGlass,
-    glassEye,
-    teleporter,
-    spire,
-    armil,
-    spikeTrap,
-    blinkTrap,
-    freezeTrap,
-    soundTrap,
-    gateTrigger,
-    stone,
-    mouse,
-    cursor,
-    energy,
-    herb,
-    grass,
-    statue,
-    smoke,
-    magnifier,
-    other,
-};
